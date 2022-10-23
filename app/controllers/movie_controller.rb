a65d1a6ff33065dc0d4fda5c634b78ba74036a8a
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2022-10-23 23:36:24 +0100

    routes added

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,7 +1,12 @@
-class MovieController < ApplicationController
+class MoviesController < ApplicationController
+  
   def index
+    render json: "Hello world"
   end
 
   def show
+
+    movie_id = params [:id]
+    render json: Example of a movie with id #{movie_id}""
   end
 end
