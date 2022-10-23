e7f5b312a856f6971b7b339b0279a830a8843542
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2022-10-24 00:29:36 +0100

    fetch all data from swapi

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,7 @@
 class MoviesController < ApplicationController
   
 def index
-data = {
-"movies": [
-{
-"id": 1,
-"title": 'The Shawshank Redemption',
-"link": Rails.application.routes.url_helpers.movie_path(1)
-},
-{
-"id": 2,
-"title": 'The Godfather',
-"link": Rails.application.routes.url_helpers.movie_path(2)
-}
-]
-}
+data = FetchDataFromSwapi.new('films', '').call
 
 render json: data
 end
