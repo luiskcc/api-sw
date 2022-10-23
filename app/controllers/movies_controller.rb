f9952ca0a25f14a2f9dbe9828a7a5fcfc78d51fb
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2022-10-24 00:35:23 +0100

    map meth controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,15 @@ class MoviesController < ApplicationController
 def index
 data = FetchDataFromSwapi.new('films', '').call
 
+
+data['results'].map! do |movie|
+{ id: movie['episode_id'],
+title: movie['title'],
+link: Rails.application.routes.url_helpers.movie_path(movie['episode_id']) }
+end
+
+
+
 render json: data
 end
 
