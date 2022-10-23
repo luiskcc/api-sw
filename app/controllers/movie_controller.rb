ee6c043af46929df80c1bf21d4edaa606e2008b3
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2022-10-23 23:21:47 +0100

    routes set up and controller def test

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,7 +1,11 @@
-class MovieController < ApplicationController
-  def index
+class MoviesController < ApplicationController
+  def index 
+    render json: "Hello World"
   end
 
   def show
+
+    movie_id = params [:id]
+    render json: "Example of a movie with id #{movie_id}"
   end
 end
